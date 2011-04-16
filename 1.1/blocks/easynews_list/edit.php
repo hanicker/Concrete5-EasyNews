93bd2137fb9d2c0eade405d1824657f201a84300
Nicola Moretti <info@nicolamoretti.com>
2011-04-16 18:01:01 +0200

    Reviewed folder structure, starting from 1.1

--- 1.1/blocks/easynews_list/edit.php
+++
@@ -1,8 +0,0 @@
-<?php 
-defined('C5_EXECUTE') or die(_("Access Denied."));
-Loader::model("collection_types");
-
-$this->inc('easynews_list_form.php');
-
-?>
-
--- blocks/easynews_list/add.php
+++
@@ -1,6 +0,0 @@
-<?php  
-defined('C5_EXECUTE') or die(_("Access Denied."));
-
-$this->inc('easynews_list_form.php');
-
-?>
